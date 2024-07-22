7613e5444d734abcdf2021ae5f9e39fbd3d4908a
Abdulrazak07 <abdulrazak85663@gmail.com>
2024-07-22 05:55:51 +0530

    minor code changes and update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,11 @@ end
 # Create Employees
 employees = [
   { first_name: 'Abdul', last_name: 'Razak', email: 'abdulrazak@gmail.com', designation: 'Software Engineer', department: Department.find_by(name: 'Engineering') },
-  { first_name: 'Priyanka', last_name: 'Chopra', email: 'priyanka123@gmail.com', designation: 'Marketing Manager', department: Department.find_by(name: 'Marketing') },
   { first_name: 'Celestina', last_name: 'Jothi', email: 'celetinajothin@email.com', designation: 'Accountant', department: Department.find_by(name: 'Finance') },
-  { first_name: 'Riswan', last_name: 'Ahamed', email: 'riswann@example.com', designation: 'HR Specialist', department: Department.find_by(name: 'Human Resources') }
+  { first_name: 'Riswan', last_name: 'Ahamed', email: 'riswann@example.com', designation: 'HR Specialist', department: Department.find_by(name: 'Human Resources') },
+  { first_name: 'Jasmin', last_name: 'Begam', email: 'jasmin@gmail.com', designation: 'Software Engineer', department: Department.find_by(name: 'Engineering') },
+  { first_name: 'Baanu', last_name: 'Mathi', email: 'banu11064@gmail.com', designation: 'Software Engineer', department: Department.find_by(name: 'Engineering') },
+  { first_name: 'Ravi', last_name: 'Kumar', email: 'kumar07@email.com', designation: 'Accountant', department: Department.find_by(name: 'Finance') },
 ]
 
 employees.each do |employee_data|
@@ -29,10 +31,10 @@ end
 
 # Create Bills
 bills = [
-  { amount: 100.50, bill_type: 'food', employee: Employee.find_by(email: 'john.doe@example.com') },
-  { amount: 200.75, bill_type: 'travel', employee: Employee.find_by(email: 'jane.smith@example.com') },
-  { amount: 150.00, bill_type: 'food', employee: Employee.find_by(email: 'emily.johnson@example.com') },
-  { amount: 250.25, bill_type: 'others', employee: Employee.find_by(email: 'michael.brown@example.com') }
+  { amount: 100.50, bill_type: 'food', employee: Employee.find_by(email: 'abdulrazak@gmail.com') },
+  { amount: 200.75, bill_type: 'travel', employee: Employee.find_by(email: 'kumar07@email.com') },
+  { amount: 150.00, bill_type: 'food', employee: Employee.find_by(email: 'celetinajothin@email.com') },
+  { amount: 250.25, bill_type: 'others', employee: Employee.find_by(email: 'riswann@example.com') }
 ]
 
 bills.each do |bill_data|
